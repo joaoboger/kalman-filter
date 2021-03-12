d258b5f19b31a1b059b05a3a0f2de2e3b19d0e39
Boger <joao.boger@gmail.com>
2021-03-12 17:34:01 -0300

    Code comments.

--- 2dKalmanFilter.py
+++ 2dKalmanFilter.py
@@ -3,7 +3,7 @@ import numpy as np
 def kalman2d(n,dt,p_v,q,Z):
     x=0 #Initial position of the robot, in the origin
     y=0
-    vx=Z[0]/math.sqrt(Z[0]**2+Z[1]**2)
+    vx=Z[0]/math.sqrt(Z[0]**2+Z[1]**2) #Initial velocities: x being cossine, and y sine
     vy=Z[1]/math.sqrt(Z[0]**2+Z[1]**2)
     px=0.005*Z[0]/math.sqrt(Z[0]**2+Z[1]**2) #Uncertainty of the initial position
     py=0.005*Z[1]/math.sqrt(Z[0]**2+Z[1]**2)
@@ -19,24 +19,25 @@ def kalman2d(n,dt,p_v,q,Z):
         #Update step#
         kgx = new_px/(new_px+Z[4*j-2]) #Kalman gain
         kgy = new_py/(new_py+Z[4*j-1])
-        kx = new_x + kgx*(Z[4*j-4]-new_x)
+        kx = new_x + kgx*(Z[4*j-4]-new_x) #Kalman positions
         ky = new_y + kgy*(Z[4*j-3]-new_y)
-        kpx = (1-kgx)*new_px
+        kpx = (1-kgx)*new_px #Kalman uncertainties
         kpy = (1-kgy)*new_py
         print("%s    %s    %s    %s" % (kx,ky,kpx,kpy))
         x=kx
         y=ky
         px=kpx
         py=kpy
-    a=Z[0]/Z[1]
-    b=x/y
+    #a=Z[0]/Z[1]
+    #b=x/y
     #print("Iter0: %s   Iter10: %s   Error: %.1f %%" % (a,b,100*abs(a-b)/a))
     return new_x,new_y,new_px,new_py
 
-#Our model will say that the robot moves at speed 1m/s in the first quadrant of a circle
-#with 45 degrees with the positive x axis.
+#Our model will say that the robot moves at speed 1 in the first quadrant of a circle.
 
-data = np.loadtxt("100Particles_errorPhi0p05.txt",dtype=float,delimiter='\t',usecols=range(40))
+data = np.loadtxt("100Particles_errorPhi0p05.txt",dtype=float,delimiter='\t',usecols=range(40)) #Initialization of the data
+
+#Looping Kalman Filter for each particle
 for i in range(0,100):
     #print("Track %s Phi angle" % (i+1))
     kalman2d(10,1,0.005,0.005,data[i][:])
--- ConcentricKalman.C
+++ ConcentricKalman.C
@@ -1,24 +1,23 @@
 void KalmanPolar()
 {
-	int n = 1000;
-	double x[n];
+	int n = 1000; //Number of positions, 10 for each particle
+	double x[n]; //Vectors which will store positions and errors 
 	double y[n];
 	double ex[n];
 	double ey[n];
 
-	//TGraphErrors *gr = new TGraphErrors();
-
-	fstream file;
+	fstream file; //Initialization of the data
 	file.open("2dKalmanModelTrust.txt", ios::in);
-
-	for(int i=0;i<1000;i++){
+	
+	
+	for(int i=0;i<1000;i++){ //Loop taking data to vectors
 		file >> x[i] >> y[i] >> ex[i] >> ey[i];
-		//gr->SetPoint(gr->GetN(),x[i],y[i],ex[i],ey[i]);
 		std::cout << x[i] << "\t" << y[i] << "\t" << "\n";
 	}
+
 	file.close();
 
-	TGraphErrors *gr = new TGraphErrors(n,x,y,ex,ey);
+	TGraphErrors *gr = new TGraphErrors(n,x,y,ex,ey); //Graph in which data will be plotted
 
 	gr->SetTitle("Kalman Filter");
 	gr->SetMarkerStyle(kFullCircle);
@@ -26,24 +25,23 @@ void KalmanPolar()
 	gr->SetLineColor(2);
 	gr->SetLineWidth(0.1);
 
-	TCanvas *c1 = new TCanvas();
+	TCanvas *c1 = new TCanvas(); //Canvas initialization
 	c1->Draw();	
 	c1->SetCanvasSize(1000,1000);
 	c1->SetWindowSize(500,500);
 	
-	TH1F* histo = new TH1F("histo","Kalman Filter Trusting Model",1,-10.5,10.5);
+	TH1F* histo = new TH1F("histo","Kalman Filter Trusting Model",1,-10.5,10.5); //Creating histogram in order to control axis' range
 	histo->SetLineWidth(0);
 	histo->Draw();
 	histo->GetYaxis()->SetRangeUser(-10.5,10.5);	
 
-	for (int i=1;i<11;i++) {
+	for (int i=1;i<11;i++) { //Loop drawing concentric circles
 		TEllipse* e = new TEllipse(0.0,0.0,11-1*i);
 		e->Draw("SAME");	
 	}
 
-	gr->Draw("P same");
+	gr->Draw("P same"); //Drawing points over the circles
 	
-	gStyle->SetOptStat(0);
-	//gr->Draw("APE");
+	gStyle->SetOptStat(0); //Don't show histogram statistics box
 	c1->Update();
 }	
