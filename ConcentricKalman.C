62312e9ce7c3767ae0335d86bcc693cdc44a50ab
Boger <joao.boger@gmail.com>
2021-03-16 15:53:42 -0300

    Data trusting equally model and measures generated from 100Particles_errorPhi0p05.txt

--- ConcentricKalman.C
+++ ConcentricKalman.C
@@ -1,36 +1,15 @@
-void KalmanPolar()
+void ConcentricKalman()
 {
 	int n = 1000; //Number of positions, 10 for each particle
-	double x[n]; //Vectors which will store positions and errors 
-	double y[n];
-	double ex[n];
-	double ey[n];
-
-	fstream file; //Initialization of the data
-	file.open("2dKalmanModelTrust.txt", ios::in);
-	
-	
-	for(int i=0;i<1000;i++){ //Loop taking data to vectors
-		file >> x[i] >> y[i] >> ex[i] >> ey[i];
-		std::cout << x[i] << "\t" << y[i] << "\t" << "\n";
-	}
-
-	file.close();
-
-	TGraphErrors *gr = new TGraphErrors(n,x,y,ex,ey); //Graph in which data will be plotted
-
-	gr->SetTitle("Kalman Filter");
-	gr->SetMarkerStyle(kFullCircle);
-	gr->SetMarkerSize(.5);
-	gr->SetLineColor(2);
-	gr->SetLineWidth(0.1);
+	int nd = 10; //Number of data points per particle/track to be fitted
 
 	TCanvas *c1 = new TCanvas(); //Canvas initialization
 	c1->Draw();	
 	c1->SetCanvasSize(1000,1000);
 	c1->SetWindowSize(500,500);
 	
-	TH1F* histo = new TH1F("histo","Kalman Filter Trusting Model",1,-10.5,10.5); //Creating histogram in order to control axis' range
+	TH1F* histo = new TH1F("histo","Kalman Filter",1,-10.5,10.5); //Creating histogram in order to control axis' range
+
 	histo->SetLineWidth(0);
 	histo->Draw();
 	histo->GetYaxis()->SetRangeUser(-10.5,10.5);	
@@ -40,7 +19,34 @@ void KalmanPolar()
 		e->Draw("SAME");	
 	}
 
-	gr->Draw("P same"); //Drawing points over the circles
+	fstream file; //Initialization of the data
+	file.open("2dKalmanSameError.txt", ios::in);
+	
+	
+	for(int i = 0 ; i < (n/nd) ; i++){ //Loop taking data to vectors, each iteration reconstructs one particle track
+		double x[nd];	  //Vectors which will store positions and errors
+		double y[nd];
+		double ex[nd];
+		double ey[nd];
+		for (int  j = 0 ; j < nd ; j++){
+			file >> x[j] >> y[j] >> ex[j] >> ey[j];
+			std::cout << x[j] << "\t" << y[j] << "\t" << "\n";
+		}
+				
+		TGraphErrors *gr = new TGraphErrors(nd,x,y,ex,ey); //Graph initialization to plot the track
+
+		gr->SetMarkerStyle(kFullCircle);
+		gr->SetMarkerSize(.5);
+		gr->SetLineColor(2);
+		gr->SetLineWidth(0.1);
+
+		gr->Draw("P same"); //Drawing points over the circles
+
+		gr->Fit("pol1","+","L same",0,10.5); //Fits our tracks in a line (polynomial of first order). The fit must be set according to the problem being considered
+	}
+
+	file.close();
+
 	
 	gStyle->SetOptStat(0); //Don't show histogram statistics box
 	c1->Update();
